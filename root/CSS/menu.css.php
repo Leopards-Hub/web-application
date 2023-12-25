542c91ee40d7b829184dd1147b2064d996f5746b
Van Thao <levanthao@gmail.com>
2023-12-26 00:40:18 +0700

    add sponsor into menu

---
+++ root/CSS/menu.css.php
@@ -0,0 +1,18 @@
+<style>
+    .sp{
+        width: 80%;
+        height:450px;
+        display:flex;
+        justify-content: center;
+        align-items: center;
+        gap: 122px;
+        margin-top: 2em;
+    }
+
+    .sp .row {
+        display: inline-flex;
+        flex-direction: column;
+        justify-content: center;
+        align-items: center;
+    }
+</style>
