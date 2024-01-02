044f30137820d77fc096cf19e866b2d7bcc3fc3d
hue <hue.ho25@student.passerellesnumeriques.org>
2024-01-02 15:23:36 +0700

    feature login

--- controllers/logincontroller.php
+++ controllers/logincontroller.php
@@ -1,6 +1,46 @@
-<?php 
-class logincontroller{
-    public function index(){
+<?php
+
+require_once '../database/database.php';
+require_once '../models/loginmodel.php';
+
+class LoginController {
+    public function index() {
         require_once 'views/loginview.php';
     }
 }
+
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    if (isset($_POST['Username']) && isset($_POST['Password'])) {
+        $Username = $_POST['Username'];
+        $Password = $_POST['Password'];
+
+        $loginModel = new LoginModel($db);
+        $User = $loginModel->loginUser($Username, $Password);
+
+        if ($User) {
+            $_SESSION['user'] = $User;
+
+            // Redirect based on user role
+            switch ($User['role']) {
+                case 'admin':
+                    header('Location: ../views/adminview.php');
+                    break;
+                case 'customer':
+                    header('Location: /controllers/views/HomeView.php');
+                    break;
+                case 'waiter':
+                    header('Location: ../views/adminview.php');
+                    break;
+                case 'chef':
+                    header('Location: ../views/adminview.php');
+                    break;
+                default:
+                    header('Location: index.php');
+            }
+            exit();
+        } else {
+            echo "Invalid username or password";
+        }
+    }
+}
+?>
--- views/loginview.php
+++ views/loginview.php
@@ -7,37 +7,36 @@
     <title>Yummy Food Restaurant</title>
     <?php include 'root/CSS/htmlfont.css.php'; ?>
     <?php include 'root/CSS/login.css.php'; ?>
-    <?php
-    include_once 'components/linkbootstrap5.php';
-    ?>
+    <?php include_once 'components/linkbootstrap5.php'; ?>
 </head>
 
 <body>
     <?php include_once 'components/header.php'; ?>
-    <div class="container-fluid" id="container-fluid" >
+    <div class="container-fluid" id="container-fluid">
         <div class="container-fluid pb-5" id="bg-1">
             <div class="container-fluid bg-1 p-5" id="bg">
-                <?php include 'components/title.php';
-                createTitle("Login", "", "", "title-h1", "", "", "hero-title"); ?>
+                <?php include 'components/title.php'; ?>
+                <?php createTitle("Login", "", "", "title-h1", "", "", "hero-title"); ?>
             </div>
         </div>
-        <div class="container-fluid bg-2 p-5 bg-white " id="bg-2" >
+        <div class="container-fluid bg-2 p-5 bg-white" id="bg-2">
         </div>
         <div class="container">
-            <form action="" method="post" >
-                <div class="row">
-                    <label for="name" class="col-form-label custom-label">Username</label>
-                    <input type="text" class="form-control rounded-pill" name="username" id="name" placeholder="Enter the username">
-                </div>
-                <div class="row">
-                    <label for="password" class="col-form-label custom-label">Message</label>
-                    <input type="password" class="form-control rounded-pill" name="password" id="password" placeholder="Enter the password">                </div>
-                <div class="row d-flex flex-column align-items-center justify-content-center pt-4 text-center">
-                    <p class="" >You don't have an account?<a href="<?php URL_ROOT.URL_SUBFLODER;?>registercontroller">Register now</a></p>
-                    <?php createButton("Login", "id-login", "send active") ?>
-                </div>
-                </div>
-            </form>
+                <form action="../controllers/logincontroller.php" method="post">
+            <div class="row">
+                <label for="Username" class="col-form-label custom-label">Username</label>
+                <input type="text" class="form-control rounded-pill" name="Username" id="Username" placeholder="Enter the username">
+            </div>
+            <div class="row">
+                <label for="Password" class="col-form-label custom-label">Password</label>
+                <input type="password" class="form-control rounded-pill" name="Password" id="Password" placeholder="Enter the password">
+            </div>
+            <div class="row d-flex flex-column align-items-center justify-content-center pt-4 text-center">
+                <p class="">You don't have an account?<a href="#">Register now</a></p>
+                <button type="submit" class="btn btn-primary">Login</button>
+            </div>
+        </form>
+
         </div>
     </div>
     <?php include_once 'components/footer.php'; ?>
